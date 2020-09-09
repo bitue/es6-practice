ce327e593910d6c33624b11828beb0dd9ca900ce
bitueboss <69850346+bitueboss@users.noreply.github.com>
2020-09-10 00:31:17 +0600

    boss

---
+++ es6.js
@@ -0,0 +1,26 @@
+// for future learn this will be more helpfull for your programming life though
+
+
+// default false value
+// empty string ""
+// 0
+// NaN
+//undefined (the value you didnot defined it yet that is undefined as well as false)
+//null
+//false
+
+//true value default
+//empty object
+//empty array
+//true
+
+const name = 0;
+if (name ){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+
+
+
